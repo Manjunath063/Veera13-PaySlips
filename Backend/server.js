efd3342fa0944a78e2b2d6f66708f514c8522c62
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-02 17:10:35 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -284,5 +284,5 @@ app.get('/api/tax-records/:month', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://16.171.226.132:${port}`);
+    console.log(`Server running at http://13.60.173.13:${port}`);
 });
